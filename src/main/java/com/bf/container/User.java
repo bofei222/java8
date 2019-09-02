4bccda2613d2106d1ff5768b0ba4a084c84a113a
bofei <bofei222@163.com>
2019-09-02 18:23:28 +0800

    List

---
+++ src/main/java/com/bf/container/User.java
@@ -0,0 +1,14 @@
+package com.bf.container;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: bofei
+ * @date: 2019-09-02 10:41
+ **/
+@Data
+public class User {
+    String groupName;
+    String employeeName;
+}
