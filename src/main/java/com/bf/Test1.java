e97d52c06d2f7acb7ecac8acac537d8aea9e4784
bofei <bofei222@163.com>
2019-12-30 11:16:06 +0800

    list of list to list of map

--- src/main/java/com/bf/Test1.java
+++ src/main/java/com/bf/Test1.java
@@ -1,8 +1,5 @@
 package com.bf;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,25 +9,9 @@ import java.util.stream.Collectors;
  * @date: 2019-12-03 14:12
  **/
 public class Test1 {
-    public static void main(String[] args) {
-
-        List<List> list = new ArrayList<>();
-        list.add(new ArrayList(Arrays.asList("timePoint", "P0000035", "P0000036")));
-        list.add(new ArrayList(Arrays.asList("1","111","222")));
-        list.add(new ArrayList(Arrays.asList("2","333","444")));
-
-        System.out.println(list);
-        List<Map> mapList = listOfList2ListOfMap(list);
 
 
-        List<Map> mapBigList = listOfMap2BigListOfMap(mapList);
-        System.out.println(mapBigList);
-        List<Map> maps = bigListOfMap2ListOfMap(mapBigList);
-        System.out.println(maps);
-
-    }
-
-    public static List<Map> listOfMap2BigListOfMap(List<Map> mapList) {
+    public static List<Map> b2A(List<Map> mapList) {
         List<Map> mapBigList = new ArrayList<>();
         for (Map map : mapList) {
             Set set = map.keySet();
@@ -48,20 +29,44 @@ public class Test1 {
         return mapBigList;
     }
 
-    // 原个数 = 大List map个数/2
-    public static List<Map> bigListOfMap2ListOfMap(List<Map> mapBigList) {
+    // 原个数 = timePoint组的个数
+    public static List<Map> a2B(List<Map> mapBigList) {
         List<Map> mapList = new ArrayList<>();
-        for (Map map : mapBigList) {
-            Map target = new HashMap();
-            target.put("timePoint", map.get("timePoint"));
-            target.put(map.get("indexCode"), map.get("indexVal"));
-            mapList.add(target);
+        Map<Object, List<Map>> timePoint = mapBigList.stream().collect(Collectors.groupingBy(e -> e.get("timePoint")));
+//        System.out.println("timePint" + timePoint);
+
+        Set<Object> keys = timePoint.keySet();
+        for (Object key : keys) {
+            if (!"timePoint".equals(key)) {
+                Map target = new HashMap(); // map的个数
+                target.put("timePoint", key);
+                List<Map> maps = timePoint.get(key);
+                for (Map map : maps) {
+                    // 非timePoint的 key的个数
+                    target.put(map.get("indexCode"), map.get("indexVal"));
+                }
+                mapList.add(target);
+            }
         }
         return mapList;
     }
 
+    public static List<List> b2C(List<Map> mapList) {
+        List<List> listList = new ArrayList<>();
 
-    public static List<Map> listOfList2ListOfMap(List<List> list) {
+        Map map = mapList.get(0);
+        Set set = map.keySet();
+        listList.add(new ArrayList(set)); // key作为第一行
+
+        for (int i = 0; i < mapList.size(); i++) {
+            List list = new ArrayList(mapList.get(i).values());
+            listList.add(list);
+        }
+        return listList;
+    }
+
+
+    public static List<Map> c2B(List<List> list) {
         List<Map> mapList = new ArrayList<>();
 
         List keyList = new ArrayList();
@@ -82,4 +87,69 @@ public class Test1 {
         System.out.println(mapList);
         return mapList;
     }
+
+    public static void main(String[] args) {
+
+        List<List> list = new ArrayList<>();
+        list.add(new ArrayList(Arrays.asList("timePoint", "P0000035", "P0000036")));
+        list.add(new ArrayList(Arrays.asList("1", "111", "222")));
+        list.add(new ArrayList(Arrays.asList("2", "333", "444")));
+
+        System.out.println(list);
+        List<Map> mapList = c2B(list);
+
+
+        List<Map> mapBigList = b2A(mapList);
+        System.out.println(mapBigList);
+        List<Map> maps = a2B(mapBigList);
+        System.out.println(maps);
+
+        List<List> lists = b2C(maps);
+        System.out.println(lists);
+
+    }
 }
+
+
+/* 此 3种 数据格式 的转换
+---------------------------A
+[{
+        "timePoint": 1,
+        "indexCode": "P0000035",
+        "indexVal": "111"
+        },
+        {
+        "timePoint": 1,
+        "indexCode": "P0000036"
+        "indexVal": "222"
+        },
+        {
+        "timePoint": 2,
+        "indexCode": "P0000035"
+        "indexVal": "333"
+        }, {
+        "timePoint": 2,
+        "indexCode": "P0000036"
+        "indexVal": "444"
+        }
+        ]
+        --------------------------B
+        [{
+        "timePoint": 1,
+        "P0000035": "111",
+        "P0000036": "222",
+        },
+        {
+        "timePoint": 2,
+        "P0000035": "333",
+        "P0000036": "444",
+        }
+
+        ]
+        --------------------------C
+        [
+        ["timePoint","P0000035","P0000036"],
+        ["1","111","222"],
+        ["2","333","444"],
+        ]
+*/
