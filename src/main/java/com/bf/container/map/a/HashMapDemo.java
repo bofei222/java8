3bd1cb7f3ad1f67038e38c903dcfbd582689db07
bofei <bofei222@163.com>
2020-04-29 18:16:08 +0800

    Predicate

--- src/main/java/com/bf/container/map/a/HashMapDemo.java
+++ src/main/java/com/bf/container/map/a/HashMapDemo.java
@@ -37,17 +37,92 @@ public class HashMapDemo {
 
     }
 
-    /*final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
+    /*final HashMap.Node<K,V>[] resize() {
+        HashMap.Node<K,V>[] oldTab = table;
+        int oldCap = (oldTab == null) ? 0 : oldTab.length; // a.第一次put时 第一次进入resize(),table为null 故oldCap=0
+        int oldThr = threshold; // 还未初始化 oldThr=0
+        int newCap, newThr = 0;
+        if (oldCap > 0) {
+            if (oldCap >= MAXIMUM_CAPACITY) {
+                threshold = Integer.MAX_VALUE;
+                return oldTab;
+            }
+            else if ((newCap = oldCap << 1) < MAXIMUM_CAPACITY &&
+                    oldCap >= DEFAULT_INITIAL_CAPACITY)
+                newThr = oldThr << 1; // double threshold
+        }
+        else if (oldThr > 0) // initial capacity was placed in threshold
+            newCap = oldThr;
+        else {               // zero initial threshold signifies using defaults
+            // a.故走这里，
+            newCap = DEFAULT_INITIAL_CAPACITY; // a.1 << 4
+            newThr = (int)(DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY); // 16*0.75=12
+        }
+        if (newThr == 0) {
+            float ft = (float)newCap * loadFactor;
+            newThr = (newCap < MAXIMUM_CAPACITY && ft < (float)MAXIMUM_CAPACITY ?
+                    (int)ft : Integer.MAX_VALUE);
+        }
+        threshold = newThr; // 初始化阈值
+        @SuppressWarnings({"rawtypes","unchecked"})
+        HashMap.Node<K,V>[] newTab = (HashMap.Node<K,V>[])new HashMap.Node[newCap]; // a首次创建是16 并没有乘以负载因子
+        table = newTab; // 初始化Node数组
+        if (oldTab != null) {
+            for (int j = 0; j < oldCap; ++j) {
+                HashMap.Node<K,V> e;
+                if ((e = oldTab[j]) != null) {
+                    oldTab[j] = null;
+                    if (e.next == null)
+                        newTab[e.hash & (newCap - 1)] = e;
+                    else if (e instanceof HashMap.TreeNode)
+                        ((HashMap.TreeNode<K,V>)e).split(this, newTab, j, oldCap);
+                    else { // preserve order
+                        HashMap.Node<K,V> loHead = null, loTail = null;
+                        HashMap.Node<K,V> hiHead = null, hiTail = null;
+                        HashMap.Node<K,V> next;
+                        do {
+                            next = e.next;
+                            if ((e.hash & oldCap) == 0) {
+                                if (loTail == null)
+                                    loHead = e;
+                                else
+                                    loTail.next = e;
+                                loTail = e;
+                            }
+                            else {
+                                if (hiTail == null)
+                                    hiHead = e;
+                                else
+                                    hiTail.next = e;
+                                hiTail = e;
+                            }
+                        } while ((e = next) != null);
+                        if (loTail != null) {
+                            loTail.next = null;
+                            newTab[j] = loHead;
+                        }
+                        if (hiTail != null) {
+                            hiTail.next = null;
+                            newTab[j + oldCap] = hiHead;
+                        }
+                    }
+                }
+            }
+        }
+        return newTab;
+    }
+
+    final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
                    boolean evict) {
 
         // 声明 临时变量 Node数组、 Node， n，i
         HashMap.Node<K,V>[] tab; HashMap.Node<K,V> p; int n, i;
 
-        //
+        // table 成员Node[]数组
         if ((tab = table) == null || (n = tab.length) == 0)
-            n = (tab = resize()).length;
+            n = (tab = resize()).length; // a.第一次走这里，table 还是空
         if ((p = tab[i = (n - 1) & hash]) == null)
-            tab[i] = newNode(hash, key, value, null);
+            tab[i] = newNode(hash, key, value, null); // a.newNode（）方法创建节点 给 Node[i]
         else {
             HashMap.Node<K,V> e; K k;
             if (p.hash == hash &&
