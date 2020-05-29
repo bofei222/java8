79fc49eb6a35e7929784cd97a24c0b876f721838
bofei <bofei222@163.com>
2020-05-29 08:41:47 +0800

    AQS

---
+++ src/main/java/com/bf/concurent/Demo.java
@@ -0,0 +1,13 @@
+package com.bf.concurent;
+
+import com.bf.stringdemo.A;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Demo {
+    public static void main(String[] args) {
+        AbstractQueuedSynchronizer a;
+        ReentrantLock reentrantLock;
+    }
+}
