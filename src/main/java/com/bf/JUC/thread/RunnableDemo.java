a5b9da5bc53c2daa8243ef288b778bc8a9908f9b
bofei <bofei222@163.com>
2020-06-16 08:16:48 +0800

    匿名内部类和取消任务

---
+++ src/main/java/com/bf/JUC/thread/RunnableDemo.java
@@ -0,0 +1,39 @@
+package com.bf.JUC.thread;
+
+public class RunnableDemo  {
+    public void test() {
+
+    }
+
+    public static void main(String[] args) {
+        RunnableDemo rd = new RunnableDemo(){
+            @Override
+          public void test() {
+              System.out.println("我是重写,我不想写新类，但是我想重写");
+          }
+        };
+
+        rd.test();
+
+        Thread t = new Thread() {
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.print(i + " ");
+                }
+            }
+        };
+        t.start();
+    }
+
+    public static void main2(String[] args) {
+        Runnable r = new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.print(i + " ");
+                }
+            }
+        };
+        Thread t = new Thread(r);
+        t.start();
+    }
+}
