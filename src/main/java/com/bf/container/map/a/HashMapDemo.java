529df611de854d55a05cf607526a641cb523bd2c
bofei <bofei222@163.com>
2020-07-06 19:10:34 +0800

    逆序遍历链表

--- src/main/java/com/bf/container/map/a/HashMapDemo.java
+++ src/main/java/com/bf/container/map/a/HashMapDemo.java
@@ -5,8 +5,11 @@ import org.checkerframework.checker.units.qual.K;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * @description:
@@ -15,11 +18,23 @@ import java.util.Map;
  **/
 public class HashMapDemo {
     public static void main(String[] args) throws NoSuchMethodException, NoSuchFieldException, InvocationTargetException, IllegalAccessException {
-        HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<String, String> map = new HashMap<String, String>(3);
+//    1:    capacity0 : 1
+//        threshold1 : 1
+//  3:      capacity0 : 4
+//        threshold1 : 4
+
+//        map.put("1", "a");
         Class<?> mapType = map.getClass();
         Method capacity = mapType.getDeclaredMethod("capacity");
         capacity.setAccessible(true);
         System.out.println("capacity0 : " + capacity.invoke(map));
+
+        Field threshold1 = mapType.getDeclaredField("threshold");
+        threshold1.setAccessible(true);
+        Object o = threshold1.get(map);
+        System.out.println("threshold1 : " + o.toString());
+
 
         Field size = mapType.getDeclaredField("size");
         size.setAccessible(true);
