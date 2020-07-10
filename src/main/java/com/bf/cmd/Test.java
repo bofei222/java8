17cb379a7208365e72302a8fb32315d3112eefd9
bofei <bofei222@163.com>
2020-07-10 17:41:59 +0800

    cpu 数

---
+++ src/main/java/com/bf/cmd/Test.java
@@ -0,0 +1,13 @@
+package com.bf.cmd;
+
+/**
+ * @description:
+ * @author: bofei
+ * @date: 2020-07-10 17:14
+ **/
+public class Test {
+    public static void main(String[] args) {
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println(i);
+    }
+}
